3aa0d1cd1c2a6bdebc5f45a40cab5a87e0ccb6c5
Raphael Rochegude-- oustry <rcr4618a@ens.fsi>
2023-09-29 09:45:01 +0200

    main

--- LesGaulois2/src/personnages/Village.java
+++ LesGaulois2/src/personnages/Village.java
@@ -2,10 +2,46 @@ package personnages;
 
 public class Village {
 	private String nom;
-	public Village(String nom) {
+	private Chef chef;
+	private int nbVillageoisMaximum;
+	private Gaulois[] Villageois;
+	private int nbVillageois=0;
+	
+	public Village(String nom,int nbVM) {
 		this.nom = nom;
+		this.nbVillageoisMaximum=nbVM;
 	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
 	public String getNom() {
 		return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		this.Villageois[nbVillageois]=gaulois;
+		nbVillageois+=1;
+	}
+	
+	public Gaulois trouverHabitant(int numero) {
+		return Villageois[numero];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef"+this.chef+"vivent les légendaire gaulois :");
+		System.out.println("-"+trouverHabitant(0)+"\n");
+	}
+	
+	public static void main(String[] args) {
+		//Gaulois gaulois = village.trouverHabitant(30);
+		//le village va jusqu'a 29
+		Village village;
+		village=new Village("“Village des Irréductibles ",30);
+		Gaulois asterix;
+		asterix =new Gaulois("Asterix",8);
+		
+		village.ajouterHabitant(asterix);
 	}
 }
